a778d110761ae8c6f7605b253e5659931353127a
icon5730 <166230648+icon5730@users.noreply.github.com>
2024-08-31 20:00:25 +0300

    Add files via upload

--- Yersinia_Installer.sh
+++ Yersinia_Installer.sh
@@ -46,7 +46,7 @@ privileges(){
         exit 1
     fi
 
-    printf "$red"
+    printf "$yellow"
     figlet -f small "Yersinia Installer"
     printf "$endcolor"
 
@@ -56,27 +56,33 @@ privileges(){
 
 
 installation(){
-read -p "$(echo -e "${cyan}[?]${endcolor}${yellow} Please provide a folder path for Yersinia cloning: ${endcolor}")" yerpath
+read -p "$(echo -e "${cyan}\xE2\x98\x90${endcolor}${yellow} Please provide a folder path for Yersinia cloning: ${endcolor}")" yerpath
 sleep 0.2 
 if [ ! -z $yerpath ]
     then
-    echo -e "${cyan}[+]${endcolor}${blue} Yersinia will be cloned into ${endcolor}${magenta}$yerpath${endcolor}"
+    echo -e "${cyan}\xE2\x98\x91${endcolor}${green} Yersinia will be cloned into ${endcolor}${magenta}$yerpath${endcolor}" ; sleep 0.3
+    echo -e "${cyan}\xE2\x98\x90${endcolor}${blue} Cloning...${endcolor}"
     git clone https://github.com/tomac/yersinia $yerpath &>/dev/null
     progress_bar 10
-    echo -e "${cyan}[!]${endcolor}${green} Cloning complete${endcolor}"  ; sleep 0.3
-    echo -e "${cyan}[+]${endcolor}${blue} Installing dependencies...${endcolor}"
+    echo -e "${cyan}\xE2\x98\x91${endcolor}${green} Cloning complete${endcolor}"  ; sleep 0.3
+    echo -e "${cyan}\xE2\x98\x90${endcolor}${blue} Installing dependencies...${endcolor}"
     apt install autoconf libgtk-3-dev libnet1-dev libgtk2.0-dev libpcap-dev -y &>/dev/null
     progress_bar 20
-    echo -e "${cyan}[!]${endcolor}${green} Dependency installation complete${endcolor}" ; sleep 0.3
-    echo -e "${cyan}[+]${endcolor}${blue} Configuring and installing Yersinia...${endcolor}"
+    echo -e "${cyan}\xE2\x98\x91${endcolor}${green} Dependency installation complete${endcolor}" ; sleep 0.3
+    echo -e "${cyan}\xE2\x98\x90${endcolor}${blue} Configuring and installing Yersinia...${endcolor}"
     cd $yerpath
     ./autogen.sh &>/dev/null
     make &>/dev/null
     make install &>/dev/null
     progress_bar 30
-    echo -e "\n${green}[!] Installation complete${endcolor}"
+    if ! command -v yersinia &>/dev/null
+	then
+	echo -e "${red}\xE2\x9A\xA0 Yersinia installation failed${endcolor}"
+	else
+    	echo -e "\n${green}\xE2\x98\x91 Yersinia installation complete${endcolor}"
+    fi
     else
-    echo -e "${red}[!] No path was specified${endcolor}"
+    echo -e "${red}\xE2\x9A\xA0 No path was specified${endcolor}"
     sleep 0.5 ; installation
 fi
 
